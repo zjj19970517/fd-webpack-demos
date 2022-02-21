64d24468d758ed2c98272940a79c6496ab79d590
zhangjinjie <zhangjinjie@luojilab.com>
2022-02-21 22:02:33 +0800

    feat: 分析css打包

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,17 @@ const config = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
   }
 };
 
